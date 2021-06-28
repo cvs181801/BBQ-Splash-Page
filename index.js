ea9a6b0e1e806003f8439fc04978dea29e3f93de
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-28 16:56:34 -0700

    add in the email from the form

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ emailCollectorForm.addEventListener("submit", e => {
     console.log("it worked!!");
     let ourFormData = new FormData(e.target);
     const userFirstname = ourFormData.get("name");
+    const userEmailAddress = ourFormData.get("emailAddress");
     const updatedHTML = `<h1 class="secondary-header">Congratulations, ${userFirstname}!</h1>
            <h2> Your registration is complete. <br>
            <br>
-           You're on your way to becoming a BBQ Master.</h2>`;
+           You're on your way to becoming a BBQ Master.</h2>
+           <p class="fine-print">You will get weekly BBQ tips sent to: ${userEmailAddress}</p>`;
         let mainContent = document.getElementById("smaller-div");
         mainContent.innerHTML = updatedHTML;   
 })
