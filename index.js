b7421c3d1afb8aeb2516ca3357606f9962bc0801
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-28 16:49:36 -0700

    add id to the html element as well

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ emailCollectorForm.addEventListener("submit", e => {
     console.log("it worked!!");
     let ourFormData = new FormData(e.target);
     const userFirstname = ourFormData.get("name");
-    const updatedHTML = `<h1 class="secondary-header">Congratulations, ${userFirstname}! </h1>
-           <p> You're on your way to becoming a BBQ Master.</p>`;
+    const updatedHTML = `<h1 class="secondary-header">Congratulations, ${userFirstname}!</h1>
+           <p class="thin-text"> You're on your way to becoming a BBQ Master.</p>`;
         let mainContent = document.getElementById("smaller-div");
         mainContent.innerHTML = updatedHTML;   
 })
