b434b58280c84707f884b078c965399af448d9b2
cvs181801 <79177146+cvs181801@users.noreply.github.com>
2021-06-28 16:25:07 -0700

    add event listener

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 //grab the form element
 let emailCollectorForm = document.getElementById("Email-Collector")
 console.log(emailCollectorForm);  
+
+emailCollectorForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("it worked!!");
+})
